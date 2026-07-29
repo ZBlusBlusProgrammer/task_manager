2121435af2ac1e66f1509d6a678947f55d8ef3c8
ZBlusBlusProgrammer <g.abandah@gmail.com>
2026-07-29 12:39:09 +0300

    init: configure django project, tasks app, and postgresql connection

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_manager_db',
+        'USER': 'postgres',        # Replace with your PostgreSQL username if different
+        'PASSWORD': 'GhpgAdmin', # Replace with your PostgreSQL password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
